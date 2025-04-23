766526c01306311dfe2a6caab240f0e88b181cd1
j3kethomas <thomasjake713@gmail.com>
2025-04-23 12:09:23 -0400

    update

--- edgetrailresearch/app.py
+++ edgetrailresearch/app.py
@@ -1,9 +1,12 @@
 from dash import Dash,html
+from dash_bootstrap_components.themes import BOOTSTRAP
 
-app = Dash()
-
-app.layout = [
-    html.Div(children="Hello")
-]
+def main():
+    app = Dash(__name__, external_stylesheets= [BOOTSTRAP])
+    app.layout = html.H1(
+        children = 'Welcome to Edgetrail Terminal'
+    )
+    app.run()
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    main() 
